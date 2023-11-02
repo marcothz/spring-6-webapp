3d89bebafe2c859cc1c4770e04460193a0dd6435
Marco Thomazini <marcothz@gmail.com>
2023-11-02 20:42:03 -0300

    feat: 20. initializing data with spring

--- src/main/java/dev/marcothz/springframework/spring6webapp/domain/Author.java
+++ src/main/java/dev/marcothz/springframework/spring6webapp/domain/Author.java
@@ -2,6 +2,7 @@ package dev.marcothz.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -13,7 +14,7 @@ public class Author {
     private String firstName;
     private String lastName;
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Long getId() {
         return id;
